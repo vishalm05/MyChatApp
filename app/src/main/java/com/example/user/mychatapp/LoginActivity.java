894356cbc85d79d5a920c72374803fc55ad6992d
Vishal <vishal@github.com>
2018-03-18 16:11:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/user/mychatapp/LoginActivity.java
@@ -0,0 +1,93 @@
+package com.example.user.mychatapp;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private android.support.v7.widget.Toolbar mToolbar;
+
+    private TextInputLayout mLoginEmail;
+    private TextInputLayout mLoginPassword;
+    private Button mLogin_btn;
+
+    private ProgressDialog mLoginProgress;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mToolbar = ( android.support.v7.widget.Toolbar ) findViewById(R.id.login_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Login");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mLoginProgress = new ProgressDialog(this);
+
+        mLoginEmail = ( TextInputLayout ) findViewById(R.id.login_email);
+        mLoginPassword = ( TextInputLayout ) findViewById(R.id.login_password);
+        mLogin_btn = ( Button ) findViewById(R.id.login_btn);
+
+        mLogin_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = mLoginEmail.getEditText().getText().toString();
+                String password = mLoginPassword.getEditText().getText().toString();
+
+                if (!TextUtils.isEmpty(email) || !TextUtils.isEmpty(password)) {
+                    mLoginProgress.setTitle("Logging In");
+                    mLoginProgress.setMessage("Please wait while we check your credentials.");
+                    mLoginProgress.setCanceledOnTouchOutside(false);
+                    mLoginProgress.show();
+
+                    loginUser(email, password);
+                }
+            }
+        });
+    }
+
+    private void loginUser(String email, String password) {
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+
+                    mLoginProgress.dismiss();
+
+                    Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(mainIntent);
+                    finish();
+
+                } else {
+
+                    mLoginProgress.hide();
+
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(LoginActivity.this, "Cannot Sign In. Please check the details and try again.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/user/mychatapp/StartActivity.java
@@ -0,0 +1,38 @@
+package com.example.user.mychatapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    private Button mRegBtn;
+    private Button mLoginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        mRegBtn = ( Button ) findViewById(R.id.start_reg_btn);
+        mRegBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent reg_intent = new Intent(StartActivity.this, RegisterActivity.class);
+                startActivity(reg_intent);
+            }
+        });
+
+        mLoginBtn = ( Button ) findViewById(R.id.login_btn);
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent login_intent = new Intent(StartActivity.this, LoginActivity.class);
+                startActivity(login_intent);
+            }
+        });
+    }
+}
